43c75b2016cb2d56ec67680b49e544523f753286
fruitmonkey01 <ericfeng35@yahoo.com>
2022-01-26 12:07:05 -0800

    Update UserInputService_Mouse.lua
    Update code for generic part example

--- UserInputService_Mouse.lua
+++ UserInputService_Mouse.lua
@@ -1,33 +1,24 @@
- -- Topic: How to Display an Items Name with Mouse Hover - Roblox Studio Tutorial
+-- Topic: How to Display an Items Name with Mouse Hover - Roblox Studio Tutorial
 -- Reference Source on the YouTube: https://www.youtube.com/watch?v=_qAcPiFAkm0
 
 local UIS = game:GetService("UserInputService")
-loca player = game.Players.LocalPlayer
+local player = game.Players.LocalPlayer
 local mouse = player:GetMouse()
 
-local ITT = Instance.new("InvisibleTouchTarget")
-ITT.Parent = game.Workspace
-
--- local ITT_Object = game.Workspace:waitForChild("InvisibleTouchTarget")
+local Part = Instance.new("Part")
+Part.Parent = game.Workspace
+Part.Name = "Part"
+-- local Part_Object = game.Workspace:waitForChild("Part")
 
 UIS.inputChanged:Connect(function(input)
 	-- check if mouse points to an object
 	if mouse.Target then 
-		if mouse.Target:FindFirstChild('InvisibleTouchTarget') then
-			-- Option 1: Disable the ITT object
-			if mouse.Target.Name == ITT.Name then
-				ITT.Enabled = false
-				print("ITT Disabled!")
-			end
-			-- Option 2: Delete the ITT object 
-			if mouse.Target.Name == ITT.Name then
-				ITT:Destroy()
-				print("ITT Deleted!")
-			end	
-		else
-			-- print something else
-			print("mouse.Target.Name" .. tostring(mouse.Target.Name))
-		end
+		print("mouse.Target.Name = [" .. tostring(mouse.Target.Name) .. "]")
+		-- Delete the Part object 
+		if mouse.Target.Name == Part.Name then
+			print("Delete " .. tostring(Part.Name))
+			Part:Destroy()
+		end	
+			
 	end
 end)
-	
